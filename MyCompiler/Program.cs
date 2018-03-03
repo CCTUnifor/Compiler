f07bfac21695c315b4b06f157cd145036095dc5f
Thiago Maia <thiago.maia971@gmail.com>
2018-03-03 09:36:28 -0300

    Initial

--- MyCompiler/Program.cs
+++ MyCompiler/Program.cs
@@ -1,35 +1,50 @@
 ﻿using System;
+using System.Collections.Generic;
 using MyCompiler.Core.Enums.RegularExpression;
 using MyCompiler.Core.Interfaces;
 using MyCompiler.Core.Models.LexicalAnalyzer;
 
-namespace MyCompiler
+namespace MyCompiler.ConsoleApp
 {
     public class Program
     {
         private static void Main(string[] args)
         {
-            //ILexicalAnalyzer<MathExpressionGrammarClass> lexicalAnalyzer = new MathExpressionLexicalAnalyzer();
-            ILexicalAnalyzer<RegularExpressionGrammarClass> lexicalAnalyzer = new RegularExpressionLexicalAnalyzer();
-
             try
             {
                 Console.WriteLine("Write your Input: ");
                 var input = Console.ReadLine();
+
+                //ILexicalAnalyzer<MathExpressionGrammarClass> lexicalAnalyzer = new MathExpressionLexicalAnalyzer();
+                var lexicalAnalyzer = new RegularExpressionLexicalAnalyzer();
+                var sintaxAnalyzer = new RegularExpressionSintexAnalyzer();
+
                 var tokens = lexicalAnalyzer.LoadTokens(input);
 
-                Console.WriteLine("\n-----------------------------------------------------\n");
-                Console.WriteLine("Result: ");
-                foreach (var token in tokens)
-                    Console.WriteLine($"{token.Value.PadRight(10)} - {token.GrammarClass}");
 
+                PrintTokens(tokens);
+                Console.ReadLine();
             }
             catch (Exception e)
             {
                 Console.WriteLine(e);
             }
-
-            Console.ReadLine();
         }
+
+        private static void PrintTokens(IEnumerable<IToken<RegularExpressionGrammarClass>> tokens)
+        {
+            Console.WriteLine("\n-----------------------------------------------------\n");
+            Console.WriteLine("Result: ");
+            foreach (var token in tokens)
+                Console.WriteLine($"{token.Value.PadRight(10)} - {token.GrammarClass}");
+        }
+    }
+
+    public interface ISintexAnalyzer
+    {
+    }
+
+    public class RegularExpressionSintexAnalyzer : ISintexAnalyzer
+    {
     }
 }
