b11c7ee64a572e0f03547960c5d9e109410ae38a
Daniel Aragão Abreu Filho <danielfilhoce@gmail.com>
2018-03-13 12:07:45 -0300

    versão não funcional do subsetBuilder para tirar dúvidas com o professor

--- ER para AFD mínimo/python/ER_AFD.py
+++ ER para AFD mínimo/python/ER_AFD.py
@@ -1,5 +1,6 @@
 from Lexical import LexicalAnalyzer as LxA
 from Sintatic import SintaticAnalyzer as StA
+from subsetsBuilder import Builder as SBuilder
 import ThompsonPrinter as printer
 
 # http://matt.might.net/articles/parsing-regex-with-recursive-descent/
@@ -8,7 +9,7 @@ import ThompsonPrinter as printer
 
 if( __name__ == "__main__"):
     # entry = '(e|d)+'
-    entry = 'a(e|d)*'
+    entry = 'e(e|d)*'
     # entry = '78d2 abc d 527000'
     # entry = '(a|b)|c'
     # entry= '(|c)*' # erro
@@ -21,4 +22,8 @@ if( __name__ == "__main__"):
 
     printer.printTable(graph)    
     printer.printMatplotlib(graph)
+
+    # sb = SBuilder(graph)
+    # sb.build()
+
     
---
+++ ER para AFD mínimo/python/subsetsBuilder.py
@@ -0,0 +1,68 @@
+from Entidades.Node import ThompsonGraph
+
+
+class Subset:
+    def __init__(self, id, alphabet):
+        self.id = id
+        self.states = []
+        self.alphabetSets = {}
+        for c in alphabet:
+            self.alphabetSets[c] = []
+
+
+class Builder:
+    def __init__(self, Graph: ThompsonGraph):
+        self.graph = Graph
+        self.alphabet = self.get_alphabet()
+        self.subsets = []
+        self.subsetId = 65
+        self.cursor = Graph.root
+    
+    def get_alphabet(self):
+        alphabet = []
+        for edge in self.graph.edge_list:
+            if(edge.value is not None):
+                alphabet.append(edge.value.value)
+        
+        return alphabet
+    
+    def getNewSubset():
+        # Ascii code
+        charId = str(unichr(self.subsetId))
+        self.subsetId += 1
+        if(self.subsetId is 91):
+            self.subsetId = 97
+        elif(self.subsetId is 123):
+            self.subsetId = 145
+        
+        return Subset(charId, self.alphabet)
+    
+    def build(self):
+        subset = self.getNewSubset()
+        alphabetSets = {
+            'done': []
+            'undone':[]
+        }
+        while(True):
+            self.recursiveFill(subset, self.cursor)
+
+            subset.alphabetSets
+
+            if (len(alphabetSets['undone']) is 0):
+                break
+
+    @staticmethod
+    def recursiveFill(subset, node):
+        subset.states.append(node)
+
+        for path in node.paths:
+            if(path.value is None):
+                if(path.dest not in subset.states):
+                    recursiveFill(subset, path.dest)
+            else:
+                token = path.value
+                parei aqui: quando encontro um valor, devo adicionar o último estado de valor ou os dois estados?
+                # recursiveFillSemiFunction(subset, token.value, path.dest)
+
+                # alphabetSet = subset.alphabetSets[token.value]
+                # alphabetSet.append(path.dest)
