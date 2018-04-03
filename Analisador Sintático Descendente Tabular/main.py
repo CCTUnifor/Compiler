13e7506cb32f0d975e2b0c104b2c49920bc74dcb
Daniel <danielfilhoce@gmail.com>
2018-04-03 20:13:40 -0300

    aplicação da 'fatoração da gramática' para finalizar a fase 6

--- Analisador Sintático Descendente Tabular/main.py
+++ Analisador Sintático Descendente Tabular/main.py
@@ -7,11 +7,12 @@ import printer
 input_file_directory = "arquivos/inputs/input "
 grammar_file_directory = "arquivos/gramaticas/Gramática "
 
+grammar_name = "EB"
+grammar_name = "Tiny2"
 grammar_name = "EABCD"
 grammar_name = "SXYZ"
-grammar_name = "EB"
+grammar_name = "SAB"
 grammar_name = "ETF"
-grammar_name = "Tiny2"
 grammar_name = "Tiny"
 
 grammar_file_name = grammar_file_directory + grammar_name
@@ -25,15 +26,17 @@ with io.open(grammar_file_name, "r", encoding='utf8') as file_obj:
     fileTxt = file_obj.read()
     g = Grammar.Grammar(fileTxt)
 
+compileGrammarService = TableService(g)
+compileGrammarService.compileGrammar()
+
 with io.open(input_file_name, "r", encoding='utf8') as file_obj:
     fileTxt = file_obj.read()
 
-    compileGrammarService = TableService(g)
     tokens, historic = compileGrammarService.compile(fileTxt)
 
-    # printer.Grammar_Printer(g)
-    # printer.Grammar_Table_Printer(compileGrammarService)
-    # printer.LexicPrint(tokens)
-    # printer.CompileHistoric(historic)
 
+printer.Grammar_Printer(g)
+printer.Grammar_Table_Printer(compileGrammarService)
+printer.LexicPrint(tokens)
+printer.CompileHistoric(historic)
 
